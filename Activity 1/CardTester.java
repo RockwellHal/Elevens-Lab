a7d9b33d0d6d9ec2179c2f7a6580314d8cbbec6f
RockwellHal <rockwh17@polyprep.org>
2017-02-16 13:03:28 -0500

    Activity 3 + 2 + 1
    Activity 3 is done now and the actual code for 2 and 1 should be there
    too (it wasn't before).

--- Activity 1/CardTester.java
+++ Activity 1/CardTester.java
@@ -8,6 +8,13 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	    Card one = new Card("jack", "clubs", 11);
+	    Card two = new Card("joker", "spades", 1000000);
+	    Card three = new Card("king", "conquering", 1000001);
+	    Card four = new Card("jack", "clubs", 11);
+	    
+	    System.out.println(one.matches(two));
+	    System.out.println(one.matches(four));
+	    System.out.println(three.toString());
 	}
 }
--- Activity 2/DeckTester.java
+++ Activity 2/DeckTester.java
@@ -8,6 +8,18 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks = new String[] {"king", "jack", "eight", "queen", "joker", "ace"};
+		String[] suits = new String[] {"spades", "clubs", "hearts", "spades", "chaos", "hearts"};
+		int[] values = new int[] {12, 11, 8, 13, 0, 1};
+		Deck deck1 = new Deck(ranks, suits, values);
+		System.out.println(deck1);
+		System.out.println(deck1.size());
+		System.out.println(deck1.isEmpty());
+		System.out.println(deck1.deal().toString());
+		System.out.println(deck1.size());
+		for(int i = 0; i < 5; i++) {
+		    System.out.println(deck1.deal().toString());
+		    System.out.println(deck1.isEmpty());
+		  }
 	}
 }
