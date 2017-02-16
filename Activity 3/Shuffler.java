322b6cc8792407d0442911eb814e51c141df8e8f
RockwellHal <rockwh17@polyprep.org>
2017-02-16 13:10:44 -0500

    Getting rid of the gifs
    Good riddance.

--- Activity 3/Shuffler.java
+++ Activity 3/Shuffler.java
@@ -16,7 +16,7 @@ public class Shuffler {
     public static void main(String[] args) {
         System.out.println("Results of " + SHUFFLE_COUNT +
             " consecutive perfect shuffles:");
-        int[] values1 = {0, 1, 2, 3, 4, 5, 6};
+        int[] values1 = {0, 1, 2, 3};
         for (int j = 1; j <= SHUFFLE_COUNT; j++) {
             perfectShuffle(values1);
             System.out.print("  " + j + ":");
@@ -29,7 +29,7 @@ public class Shuffler {
 
         System.out.println("Results of " + SHUFFLE_COUNT +
             " consecutive efficient selection shuffles:");
-        int[] values2 = {0, 1, 2, 3, 4, 5, 6};
+        int[] values2 = {0, 1, 2, 3};
         for (int j = 1; j <= SHUFFLE_COUNT; j++) {
             selectionShuffle(values2);
             System.out.print("  " + j + ":");
